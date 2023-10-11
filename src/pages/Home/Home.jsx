7dca10ac72f4d4e87e8c77eea06144b40d0ba1ff
maheshbasnet089 <basnetmanish089@gmail.com>
2023-10-11 22:52:22 +0545

    learn apiIntegration,sop,cors

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,36 @@
+import axios from "axios"
 import Navbar from "../../components/Navbar/Navbar"
+import { useEffect,useState } from "react"
+
 
 const Home = () => {
+  const [blogs,setBlogs] = useState([])
+  // api call here 
+
+  const fetchBlogs = async ()=>{
+   const response =  await axios.get('http://localhost:2000/blogs')
+   setBlogs(response.data.blogs)
+  }
+  useEffect(()=>{
+    fetchBlogs()
+  },[])
+
+  
   return (
     <div>
       <Navbar />
       <div className="card" style={{width: '18rem'}}>
-
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <h5 className="card-title">Card subTitle</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
+    {blogs.map((blog)=>{
+      return(
+        <div key={blog._id} className="card-body">
+        <h5 className="card-title">{blog.title}</h5>
+        <h5 className="card-title">{blog.subTitle}</h5>
+        <p className="card-text">{blog.description}</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+      )
+    }) }
+ 
 </div>
     </div>
   )
