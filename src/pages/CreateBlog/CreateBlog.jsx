af8103aa688e253b9a9be63a83e328d0097856f5
maheshbasnet089 <basnetmanish089@gmail.com>
2023-10-13 22:12:01 +0545

    third approach using separate function handler

--- src/pages/CreateBlog/CreateBlog.jsx
+++ src/pages/CreateBlog/CreateBlog.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import './Createblog.css'
 import axios from 'axios'
@@ -5,14 +6,15 @@ import { useNavigate } from 'react-router-dom'
 
 const CreateBlog = () => {
     const navigate = useNavigate()
-    
-  
+    const [data,setData] = useState({
+        title : "",
+        subTitle : "",
+        description : ""
+    })
+ 
 
     const createBlog = async (e)=>{
          e.preventDefault()
-         const formData = new FormData(e.currentTarget) // {}
-        const data =  Object.fromEntries(formData)
-
          // send above states data to api 
         const response =  await axios.post("http://localhost:2000/blogs",data)
         if(response.status == 201){
@@ -23,7 +25,14 @@ const CreateBlog = () => {
         }
 
     }
+    const handleChange = (e)=>{
+        const {name,value} = e.target 
+        setData({
+            ...data,
+            [name] : value
+        })
 
+    }
   return (
     <>
         <Navbar />
@@ -32,15 +41,15 @@ const CreateBlog = () => {
     <form onSubmit={createBlog}>
         <div className="form-group">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" placeholder="Enter Title"  />
+            <input type="text" id="title" name="title" placeholder="Enter Title" onChange={handleChange}  />
         </div>
         <div className="form-group">
             <label htmlFor="subtitle">Subtitle</label>
-            <input type="text" id="subtitle" name="subTitle" placeholder="Enter Subtitle"  />
+            <input type="text" id="subtitle" name="subTitle" placeholder="Enter Subtitle"  onChange={handleChange}   />
         </div>
         <div className="form-group">
             <label htmlFor="description">Description</label>
-            <textarea id="description" name="description" rows="4" placeholder="Enter Description"  ></textarea>
+            <textarea id="description" name="description" rows="4" placeholder="Enter Description" onChange={handleChange}   ></textarea>
         </div>
         <button className="btn" type="submit">Submit</button>
     </form>
